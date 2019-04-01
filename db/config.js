a4db66a3dc0443b182334bb5efc28eb5e76c5f47
dillonbartkus <dillonbartkus@gmail.com>
2019-04-01 18:18:03 -0400

    db url

--- db/config.js
+++ db/config.js
@@ -15,8 +15,15 @@ const options = {
       host: 'localhost',
     });
   } else if (process.env.NODE_ENV === 'production') {
-    let url = `postgres://localhost:5432/logai`
-    db = pgp(url);
+    // let url = `postgres://localhost:5432/logai`
+    // db = pgp(url);
+    db = pgp({
+      host: '/cloudsql/logai-233920:us-east1:users',
+      user: 'postgres',
+      password: 'truckingrmmd19',
+      database: 'users'
+      // port: 5432,
+    });
   }
   
   module.exports = db;
