570ceb7538ab97089c427d7c53710303f3a4ea62
dillonbartkus <dillonbartkus@gmail.com>
2019-04-20 19:28:58 -0400

    copy writing changes

--- client/src/Main.js
+++ client/src/Main.js
@@ -21,7 +21,7 @@ const Main = () => {
 
                 <h4>What We Do</h4>
 
-                <p>Our end-to-end software lets you run all of your business operations in one convenient place. Stop drowning in manually-intensive spreadsheets to do financial reporting, jumping around websites to find work, and being glued to your phone and email inbox to coordinate operations and update your clients of your progress. Our logistical platform empowers you to eliminate manual and error-prone work, streamline communication, and help you work smarter.</p>
+                <p>Our end-to-end software lets you run all of your business operations in one convenient place. Stop drowning in manually-intensive spreadsheets for financial reporting, jumping around websites to find work, and being glued to your phone and email inbox to coordinate operations and update your clients of your progress. Our logistical platform empowers you to eliminate manual and error-prone work, streamline communication, and help you work smarter.</p>
 
             </div>
 
@@ -31,7 +31,7 @@ const Main = () => {
 
               <h4>Why We Do It</h4>
 
-              <p>We really care about you, appreciate your hard work, and want to help you. We know first-hand all the hard work you put in everyday to conduct logistical operations and we want to put the best technology solutions in your hands so you can work better.</p>
+              <p>We care about you, appreciate your hard work, and want to help you. We know first-hand all the hard work you put in everyday to conduct logistical operations and we want to put the best technology solutions in your hands so you can work better.</p>
             
             </div>
 
@@ -77,7 +77,7 @@ const Main = () => {
                     <li>Bridge the gap between logical planning and management</li>
                     <li>Eliminate up to 70% of your manual operations and the errors that come with it</li>
                     <li>Use real-time visualized analytical data to make proactive and efficient business decisions</li>
-                    <li>Effortlessly cater to last changes to cargo orders</li>
+                    <li>Effortlessly cater to last minute changes to cargo orders</li>
                     <li>Easily communicate with your peers</li>
                     <li>Coordinate logistical movements down to the most finite detail</li>
                     <br></br>
--- client/src/Services.js
+++ client/src/Services.js
@@ -14,7 +14,7 @@ const Services = () => {
 
               <h3>Shipping and Warehouse Solutions</h3>
 
-              <h4>Manage and transport your goods in a more simple, transparent, coordinated and smarter way, in real-time and in one place.</h4>
+              <h4>Manage and transport your goods in a simpler, transparent, coordinated and smarter way, in real-time and in one place.</h4>
 
               <p>Our end-to-end logistics platform will empower you to:</p>
 
@@ -22,19 +22,17 @@ const Services = () => {
 
               <li>Manage your inventory and orders by easily catering to last-minute & customized orders in real-time with our integrated, smart and user-friendly warehouse management (WM) and enterprise resource planning (ERP) systems</li>
 
-              <li>Find reliable carriers, review their profiles / customer reviews, and easily reach out to them to team-up and coordinate the transportation of your goods</li>
+              <li>Find reliable carriers, review their profiles and customer reviews, and easily engage them to coordinate the transportation of your goods</li>
 
-              <li>Enjoy a higher level of transparency and reduce over 70% of your work time spent on phone and emails by using our real-time progress reporting feature, which will allow you to share and see your counterparts’ progress, while also sharing this level of visibility with your customers</li>
+              <li>Enjoy a higher level of transparency and reduce over 70% of time spent on phone and email using our real-time progress reporting feature, which will allow you to view your counterparts’ progress, while also sharing this level of visibility with your customers</li>
 
-              <li>Easily coordinate and communicate with everyone in our platform by using our built-in messaging feature</li>
+              <li>Easily communicate with everyone in our platform by using our built-in messaging feature</li>
 
-              <li>Make more efficient and proactive business decisions by using our custom-made data analytics dashboard, which will provide you with real-time insights for you to action on</li>
+              <li>Make more efficient and real-time business decisions using our custom-made data analytics dashboard</li>
 
-              <li>Provide a higher level of customer service to your clients by leveraging our platform and its ability to use big data, business analytics and artificial intelligence</li>
+              <li>Provide a higher level of customer service by leveraging our platform and its ability to use big data, business analytics and artificial intelligence</li>
 
-              <li>Automate all of your finance reports and SLA’s. Heck, you will even be able to decide when to automatically send billing information to your counterparts and clients.</li>
-
-              <li>Easily conduct your finance operations and reporting</li>
+              <li>Automate financial reporting and SLA’s, including the ability to automatically send billing information to your counterparts and clients.</li>
                 
               </ul>
              
@@ -46,7 +44,7 @@ const Services = () => {
 
               <h3>Carrier Solutions</h3>
 
-              <h4>Run your transportation business from our one-stop-shop!</h4>
+              <h4>Run your transportation business from our one-stop shop!</h4>
 
                 <p>With our end-to-end logistics platform you will be able to:</p>
 
@@ -54,11 +52,11 @@ const Services = () => {
 
                 <li>Use our simple-to-use transportation management systems (TMS) dashboard to strategize and coordinate transportation of goods with shippers, warehouse counterparts, and of course, with your drivers!</li>
 
-                <li>Enjoy a higher level of transparency and reduce over 80% of your work time spent on a phones and emails by using our real-time progress reporting feature to share your drivers’ progress and also see your counterparts’ progress, while sharing this level of visibility with your customers</li>
+                <li>Enjoy a higher level of transparency and reduce over 70% of time spent on a phones and emails by using our real-time progress reporting feature to share your drivers’ progress and also see your counterparts’ progress, while sharing this level of visibility with your customers</li>
 
-                <li>Coordinate and communicate with everyone in our platform by using our built-in messaging feature</li>
+                <li>Coordinate and communicate with everyone in our platform by using our built-in messaging features</li>
 
-                <li>Make more efficient and proactive business decisions by using our custom-made data analytics dashboard, which will provide you with real-time insights for you to action on</li>
+                <li>Make more efficient and proactive business decisions by using our custom-made data analytics dashboard, which will provide you with real-time insights</li>
 
                 <li>Provide a higher level of customer service to your clients by leveraging our platform and its ability to use big data, business analytics and artificial intelligence</li>
 
