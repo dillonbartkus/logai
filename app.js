3f3f5f9e01a772e20cdfab768e64b424f4a5bbb5
dillonbartkus <dillonbartkus@gmail.com>
2019-06-25 15:16:26 -0400

    test

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(parser.urlencoded({extended: false}));
 const logger = require('morgan');
 app.use(logger('dev'));
 
-// app.use(express.static('client/build'));
+app.use(express.static('client/build'));
 
 app.get('/', (req,res) => {
   res.send(`server`)
