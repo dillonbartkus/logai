af2be8f3ede8190688bf533320f13d5e71f41100
dillonbartkus <dillonbartkus@gmail.com>
2019-06-26 13:08:21 -0400

    changed proxy back

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8081;
+const port = process.env.HTTP_PORT || 3001;
 const cors = require('cors')
 const parser = require('body-parser');
 
