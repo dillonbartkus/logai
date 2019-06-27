eb6facadb82e1c87558fd9700526179c97f7ecce
dillonbartkus <dillonbartkus@gmail.com>
2019-06-27 17:09:11 -0400

    das

--- db/config.js
+++ db/config.js
@@ -8,13 +8,18 @@ const pgp = require('pg-promise')(options);
 
 let db;
 
-  db = pgp({
-    database : process.env.RDS_DB_NAME,
-    port     : process.env.RDS_PORT,
-    host     : process.env.RDS_HOSTNAME,
-    user     : process.env.RDS_USERNAME,
-    password : process.env.RDS_PASSWORD,
-    
+// db = pgp({
+//   database: 'logai',
+//   port: 5432,
+//   host: 'localhost',
+// });
+
+db = pgp({
+  database : process.env.RDS_DB_NAME,
+  port     : process.env.RDS_PORT,
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
 });
 
 module.exports = db;
