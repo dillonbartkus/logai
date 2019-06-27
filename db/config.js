39240a9e6c3dc6d6434e25539ed61e8f32b79b6d
dillonbartkus <dillonbartkus@gmail.com>
2019-06-27 12:48:26 -0400

    dev

--- db/config.js
+++ db/config.js
@@ -8,23 +8,10 @@ const pgp = require('pg-promise')(options);
 
 let db;
 
-if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
   db = pgp({
     database: 'logai',
     port: 5432,
     host: 'localhost',
   });
-} else if (process.env.NODE_ENV === 'production') {
-  
-  db = pgp({
-    database : process.env.RDS_DB_NAME,
-    port     : process.env.RDS_PORT,
-    host     : process.env.RDS_HOSTNAME,
-    user     : process.env.RDS_USERNAME,
-    password : process.env.RDS_PASSWORD,
-    
-});
-
-}
 
 module.exports = db;
